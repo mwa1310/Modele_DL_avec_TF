9118c17409bd7086ca5334f9ceccc554cc5d5898
wmezago@gmail.com <wmezago@gmail.com>
2025-10-18 13:47:00 +0100

    app.py added

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask , request , jsonify
+import tensorflow as tf
+import keras
+import numpy as np
+
+app = Flask(name)
+
+# Chargement du modèle Keras
+model = keras.models.load_model('mnist_model.h5')
+
+@app.route('/predict', methods =['POST'])
+def predict ():
+    data = request . json
+# Vérification des données
+    if 'image' not in data :
+        return jsonify({ 'error': 'No image provided'}), 400
+
+    image_data = np.array(data['image'])
+    # Assurez - vous que l’image est au bon format (1, 784) et normalisée
+    image_data = image_data.reshape(1, 784)
+    image_data = image_data.astype("float32") / 255.0
+
+    prediction = model.predict(image_data)
+    predicted_class = np.argmax(prediction, axis=1)[0]
+
+    return jsonify ({
+        'prediction':int(predicted_class),
+        'probabilities':prediction.tolist()
+    })
+
+if name == 'main':
+    app.run(host='0.0.0.0', port=5000)
